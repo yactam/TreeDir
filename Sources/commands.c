fe8ddf2c30b69c35051ef90b674551e812ed751a
TAMDRARI Yacine <yacinetamdrari5@gmail.com>
2023-03-31 18:17:29 +0200

    ls and basic cd

--- Sources/commands.c
+++ Sources/commands.c
@@ -2,19 +2,49 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <string.h>
 #include "../Headers/commands.h"
+#include "../Headers/string_utils.h"
+#include "../Headers/list_utils.h"
 
-/*************************************/
-void ls() {
-
+noeud* init() {
+	noeud* root = malloc(sizeof(noeud*));
+	assert(root != NULL);
+	root->est_dossier = true;
+	root->nom = "";
+	root->pere = root;
+	root->racine = root;
+	root->fils = NULL;
+	return root;
 }
 
-/************************************/
-void pwd() {
-
+/**********************************/
+void ls(noeud* n) {
+	if(n != NULL) liste(n->fils);
 }
 
-/***********************************/
-void cd(char*) {
+/**********************************/
+void pwd(noeud* n) {
 
 }
+/**********************************/
+noeud* cd(noeud* n, char* c) {
+	
+}
+
+/**********************************/
+// Pour la commande cd ..
+noeud* cd_parent(noeud* n) {
+	assert(n != NULL);
+	return n->pere;
+}
+
+/**********************************/
+// Pour la commande cd
+noeud* cd() {
+	assert(n != NULL);
+	return n->racine;
+}
+
+/*********************************/
+
